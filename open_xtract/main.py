39c2eedcdf81085b5e8330fe3173cdb54eb50f1a
Cole McIntosh <colemcintosh6@gmail.com>
2025-09-07 09:13:36 -0600

    add provider map

--- open_xtract/main.py
+++ open_xtract/main.py
@@ -2,7 +2,15 @@ from __future__ import annotations
 
 from pathlib import Path
 from langchain_openai import ChatOpenAI
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+try:
+    from .provider_map import provider_map  # For when imported as a module
+except ImportError:
+    from provider_map import provider_map  # For when run directly
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 class OpenXtract:
@@ -11,19 +19,25 @@ class OpenXtract:
     def __init__(
         self,
         model: str,
-        base_url: str | None = None,
-        api_key: str | None = None,
     ) -> None:
-        self._model_name = model
-        self._base_url = base_url
-        self._api_key = api_key
-        self._llm = self._create_llm(model, base_url, api_key)
+        self._model_string = model
+        self._llm_parts = self._get_parts()
 
-    def _create_llm(self, model: str, base_url: str | None, api_key: str | None):
-        return ChatOpenAI(model=model, base_url=base_url, api_key=api_key)
+        self._llm = self._create_llm()
+
+    def _get_parts(self):
+        parts = self._model_string.split(":")
+        self._provider = parts[0]
+        self._model = parts[1]
+        self._api_key = os.getenv(provider_map[self._provider]["api_key"])
+        self._base_url = provider_map[self._provider]["base_url"]
+        return self._provider, self._model, self._base_url, self._api_key
+
+    def _create_llm(self):
+        return ChatOpenAI(model=self._llm_parts[1], base_url=self._llm_parts[2], api_key=self._llm_parts[3])
 
     def extract(self, file_path: str | Path, schema: BaseModel):
         return self._llm.with_structured_output(schema).invoke(file_path)
 
 
-__all__ = ["OpenXtract", "main"]   
+__all__ = ["OpenXtract", "main"]
